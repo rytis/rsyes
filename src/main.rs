70b1a4da1137bd4ef2d134a9d8c3b939f3478377
Rytis Sileika <rts.pub@pulegium.eu>
2020-09-30 10:19:41 +0100

    Increase buffer, fix buffer size calculation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use std::env;
 
 fn main() {
-    const BUF_SIZE:usize = 4096;
+    const BUF_SIZE:usize = 1024*8;
 
     let default = String::from("y");
     let args: Vec<String> = env::args().collect();
     let input = if args.len() > 1 { &args[1] } else { &default };
+    let input_size = input.len() + 1;
 
     let mut output = input.clone();
     output.push('\n');
 
-    for _x in 0..(BUF_SIZE/(input.len()+1)) {
+    for _x in 0..((BUF_SIZE-input_size)/input_size) {
         output.push_str(input);
         output.push('\n');
     }
